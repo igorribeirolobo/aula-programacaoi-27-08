095a634724c373087bc1502109b3c89ce175c52d
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-08-29 21:14:32 -0300

    Initial commit

---
+++ src/Aula/Main.java
@@ -0,0 +1,11 @@
+package Aula;
+
+public class Main {
+
+	public static void main(String[] args) {
+	   funcoes f = new funcoes();
+	   
+	  System.out.println(f.converterInt());
+	}
+
+}
---
+++ src/Aula/funcoes.java
@@ -0,0 +1,28 @@
+package Aula;
+
+public class funcoes {
+	
+	public double divide()
+	{
+		double x = 9/2;
+		return x;
+	}
+	
+	public long converterlong()
+	{
+		int num1 = 55555;
+		int num2 = 66666;
+		
+		long num3 = (long)num1 * num2;
+		
+		return num3;
+	}
+	
+	public int converterInt()
+	{
+		int myInt;
+		double myPercent = 51.9;
+		
+		return myInt = (int)myPercent;
+	}
+}
